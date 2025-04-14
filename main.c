c0dbf4b40564100bde4bbeb5feb8c09f8d14f4ce
ngovannamt36@gmail.com <ngovannamt36@gmail.com>
2025-04-14 14:08:36 +0700

    su dung return cua ham main()

--- main.c
+++ main.c
@@ -31,9 +31,26 @@ void kiemTraSNT(int n){
     }
 }
 
+
 int main()
 {
-    kiemTraSNT(17);
-    
+    int n  = 67;
+
+    if (n < 2)
+    {
+        printf("%d khong phai la so nguyen to\n",n);
+    }
+    else
+    {
+        for (int i = 2; i < n/2; i++)
+        {
+            if (n % i == 0)
+            {
+                printf("%d khong phai la so nguyen to\n",n);
+                return 0;
+            }
+        }
+    }
+    printf("%d la so nguyen to\n",n);    
     return 0;
 }
