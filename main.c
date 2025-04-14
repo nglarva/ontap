10c65e5c56518993881dba239d88f594e7b188e3
ngovannamt36@gmail.com <ngovannamt36@gmail.com>
2025-04-14 14:00:45 +0700

    bai 1: kiem tra so nguyen to

---
+++ main.c
@@ -0,0 +1,39 @@
+// Kiểm tra số nguyên tố
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+int n = 29;
+bool laSoNguyenTo = true;
+
+void kiemTraSNT(int n){
+    if (n < 2 || n % 2 == 0)
+    {
+        laSoNguyenTo = false;
+    }
+    else
+    {
+        for (int i = 3; i <= sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                laSoNguyenTo = false;
+                break;
+            }
+        }
+        if (laSoNguyenTo == true)
+        {
+            printf("%d la so nguyen to\n", n);
+        }
+        else {
+            printf("%d khong la so nguyen to\n", n);
+        }
+    }
+}
+
+int main()
+{
+    kiemTraSNT(17);
+    
+    return 0;
+}
