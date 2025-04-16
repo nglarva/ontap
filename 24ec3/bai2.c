b48a4e7abcaaa1dee1d401a44e2d80afaf710a84
ngovannamt36@gmail.com <ngovannamt36@gmail.com>
2025-04-16 17:37:35 +0700

    bai3 update

--- 24ec3/bai2.c
+++ 24ec3/bai2.c
@@ -63,8 +63,9 @@
 //     printf("So ki tu hoa la: %d", countUpperCase);
 // }
 
-int main(void){
-    char chuoikitu[] = "       Hello      world        ";
+void xoaKTDauCuoi(){
+    char chuoikitu[] = "       Hello      World        ";
+    int upcount=0;
     printf("Chuoi truoc khi xoa khoang trang o dau\n");
     for(int i=0;chuoikitu[i] !='\0';i++){
         printf("chuoikitu[%d] = %c\n",i,chuoikitu[i]);
@@ -79,8 +80,62 @@ int main(void){
     while(chuoikitu[strlen(chuoikitu)-1] == ' '){
         chuoikitu[strlen(chuoikitu)-1] = '\0';
     }
+    for (int i=0; i<strlen(chuoikitu); i++){
+        if(isupper(chuoikitu[i])){
+            upcount++;
+        }
+    }
      printf("Chuoi sau khi xoa \n");
     for(int i=0;chuoikitu[i] !='\0';i++){
         printf("chuoikitu[%d] = %c\n",i,chuoikitu[i]);
     }
+    printf("so ki tu viet hoa: %d",upcount);
+}
+typedef struct SinhVien{
+    int MaSV;
+    char HoTen[30];
+    float DiemTB;
+    } SV; //alias
+
+int main(void){
+    int n;
+    
+    SV sv1={1,"Manh Hung",4.5};
+
+    printf("MaSV: %d\nHo ten: %s\nDiemTB: %.2f",sv1.MaSV,sv1.HoTen,sv1.DiemTB);
+
+    SV sv2;
+    SV dsSV[100];
+
+//     printf("Nhap maSV: ");
+//     scanf("%d", &sv2.MaSV);
+//     printf("\nNhap ho ten: ");
+//     //scanf("%s", sv2.HoTen);
+//     getchar();
+//     fgets(sv2.HoTen,30,stdin);
+//     printf("\nNhap diem trung binh: ");
+//     scanf("%f", &sv2.DiemTB);
+
+//     printf("MaSV: %d\nHo ten: %s\nDiemTB: %.2f",sv2.MaSV,sv2.HoTen,sv2.DiemTB);
+    
+    
+    
+//     return 0;
+// }
+printf("Nhap so luong sinh vien");
+scanf("%d", &n);
+for(int i = 0; i < n; i++){
+    printf("Nhap maSV: ");
+    scanf("%d", &dsSV[i].MaSV);
+    printf("Nhap ho ten: ");
+    getchar();
+    fgets(dsSV[i].HoTen,30,stdin);
+    dsSV[i].HoTen[strcspn(dsSV[i].HoTen,"\n")] = '\0';
+    printf("Nhap diem trung binh: ");
+    scanf("%f", &dsSV[i].DiemTB);
+}
+printf("Danh sach sinh vien\n");
+for(int i= 0; i<n; i++){
+    printf("%d, %s, %f\n", dsSV[i].MaSV, dsSV[i].HoTen, dsSV[i].DiemTB);
+}
 }
