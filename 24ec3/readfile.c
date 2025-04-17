eb2cae57045d7dde360f6b234be994924ba131d9
ngovannamt36@gmail.com <ngovannamt36@gmail.com>
2025-04-17 17:06:07 +0700

    bai4 file

--- 24ec3/readfile.c
+++ 24ec3/readfile.c
@@ -2,18 +2,32 @@
 
 int main(){
     int sum = 0;
-    int a = 0;
+    int n;
+    int a,b = 0;
     FILE *fptr;
+
+    // fptr = fopen("numbers.txt","w");
+    // printf("Nhap so n: ");
+    // scanf("%d", &n);
+    // if(fptr != NULL){
+    //     for(int i = 0; i < n; i++){
+    //         printf("Nhap gia tri %d: ",i+1);
+    //         scanf("%d",&a);
+    //         fprintf(fptr,"%d ",a);
+    //     }
+
+    // }
+    // fclose(fptr);
+
     fptr = fopen("numbers.txt","r");
-    if(fptr != NULL){
-        while (!feof(fptr))
-        {
-            fscanf(fptr,"%d", &a);
-            sum += a;
-        }
-
+    // while(!feof(fptr)){
+    //     fscanf(fptr,"%d",&b);
+    //     if(!feof(fptr)) sum+=b;
+    // }
+    while(fscanf(fptr,"%d",&b) != -1){
+        sum+=b;
     }
+    fclose(fptr);
+
     printf("Sum: %d\n",sum);
-
-
 }
